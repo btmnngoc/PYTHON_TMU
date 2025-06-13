3b948f2876d697b8133799f551310714fa92a4b6
Thannhngann-05 <oriaanaa0103@gmail.com>
2025-06-13 10:06:44 +0700

    Update customer_ops.py

--- modules/customer_ops.py
+++ modules/customer_ops.py
@@ -1,24 +1,43 @@
 from modules.file_io import read_customers, write_customers
 from datetime import datetime
 
-def update_customer(file_path, customer_id, new_data):
-    data = read_customers(file_path)
-    updated = False
+def update_customer(customers):
+    a = input("Nhập ID khách hàng bạn muốn cập nhật: ")
+    for i in range(len(customers)):
+        if customers[i][0] == a:
+            print("Nhập lựa chọn của bạn:")
+            print("a. Sửa Tên")
+            print("b. Sửa Email")
+            print("c. Sửa Số điện thoại")
+            print("d. Sửa Địa chỉ")
+            res = input("Nhập lựa chọn (a/b/c/d): ").lower()
 
-    for customer in data:
-        if customer["id"] == customer_id:
-            for key, value in new_data.items():
-                if value != "" and key in customer:
-                    customer[key] = value
-            customer["last_updated"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            updated = True
-            break
+            if res == 'a':
+                ten_moi = input("Nhập tên mới: ")
+                customers[i][1] = ten_moi
+                print("Đã cập nhật tên thành công!")
 
-    if updated:
-        write_customers(file_path, data)
-        print(f"✅ Đã cập nhật khách hàng ID {customer_id}")
-    else:
-        print(f"⚠️ Không tìm thấy khách hàng ID {customer_id}")
+            elif res == 'b':
+                email_moi = input("Nhập email mới: ")
+                customers[i][4] = email_moi
+                print("Đã cập nhật email thành công!")
+
+            elif res == 'c':
+                sdt_moi = input("Nhập số điện thoại mới: ")
+                customers[i][5] = sdt_moi
+                print("Đã cập nhật số điện thoại thành công!")
+
+            elif res == 'd':
+                diachi_moi = input("Nhập địa chỉ mới: ")
+                customers[i][6] = diachi_moi
+                print("Đã cập nhật địa chỉ thành công!")
+
+            else:
+                print("Thao tác không hợp lệ!")
+            return
+
+    print("Không tìm thấy khách hàng, vui lòng thao tác lại!")
+
         
 def delete_customer(file_path, customer_id):
     data = read_customers(file_path)
